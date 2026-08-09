67d45879ee31b6540a267380c4be2993e7ef56f9
reznak <ayay70080@gmail.com>
2026-08-09 18:48:25 +0500

    kostya

--- next.config.js
+++ next.config.js
@@ -32,7 +32,7 @@ const nextConfig = {
       {
         source: '/img/:path*',
         headers: [
-          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+          { key: 'Cache-Control', value: 'public, max-age=3600, must-revalidate' },
         ],
       },
       {
